f6570d7aba2c15a3157487a145ef6c0cb9a6ea8d
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-11-04 13:40:51 -0600

    Added css files for components

--- src/Body.js
+++ src/Body.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Body.css";
 
 export default function Body() {
   return <div>Information about myself</div>;
--- src/Footer.js
+++ src/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
   return <div>Follow me at instagram and email at ____, check out github</div>;
