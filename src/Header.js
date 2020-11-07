49753973f94364487177e3466da967bcec951eac
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-11-07 16:00:36 -0600

    Added fixed menu and logo

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,12 @@
 import React from "react";
+import logo from "./logo.png";
 import "./Header.css";
 
 export default function Header() {
-  return <div className="Header">Add a logo and site's navigation menu</div>;
+  return (
+    <div className="Header" id="navbar">
+      <img src={logo} alt="Kleer Coding logo" className="logo" />
+      Add a logo and site's navigation menu
+    </div>
+  );
 }
