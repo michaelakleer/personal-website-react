d912b21c4f5f4a099196172254044165bf0bb404
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-11-07 20:32:09 -0600

    Adding navigation menu

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,38 @@ export default function Header() {
   return (
     <div className="Header" id="navbar">
       <img src={logo} alt="Kleer Coding logo" className="logo" />
-      Add a logo and site's navigation menu
+      <nav>
+        <div className="row">
+          <div className="col-sm-3">
+            <p>
+              <a href="#aboutMe" className="nav-link about">
+                About me
+              </a>
+            </p>
+          </div>
+          <div className="col-sm-3">
+            <p>
+              <a href="#technologies" className="nav-link tech">
+                Technologies
+              </a>
+            </p>
+          </div>
+          <div className="col-sm-3">
+            <p>
+              <a href="#certificates" className="nav-link certificate">
+                Certificates
+              </a>
+            </p>
+          </div>
+          <div className="col-sm-3">
+            <p>
+              <a href="#projects" className="nav-link projects">
+                Projects
+              </a>
+            </p>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
