3980c2f0b4488c5d203d71b4f5ac4d44a74f2e02
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-11-08 13:43:29 -0600

    Added navbar button and added background color

--- src/Header.js
+++ src/Header.js
@@ -14,7 +14,18 @@ export default function Header() {
             alt="Kleer Coding logo"
           />
         </a>
-        <div className="navbar-collapse" id="navbarNav">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
               <a className="nav-link about" href="#aboutMe">
