c76bb27153d514f99fb949b9e031ef744e24288e
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-11-10 18:36:20 -0600

    Added Home.js and css

--- src/Body.js
+++ src/Body.js
@@ -1,16 +1,15 @@
 import React from "react";
+import Home from "./Home";
+import Projects from "./Projects";
 import Technologies from "./Technologies";
 import Certificates from "./Certificates";
-import Projects from "./Projects";
 
 import "./Body.css";
 
 export default function Body() {
   return (
     <div className="Body">
-      <h1>Michaela Kleer</h1>
-      <h2>Frontend React Web Developer</h2>
-      <div>Brief paragraph about myself</div>
+      <Home />
       <Projects />
       <Technologies />
       <Certificates />
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <h1>Michaela Kleer</h1>
+      <h2>Frontend React Web Developer</h2>
+      <div>Brief paragraph about myself</div>
+    </div>
+  );
+}
