df6198070397b67ff2671aaf2d0a0ef29c46a32e
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-11-11 15:21:19 -0600

    Added columns

--- src/Projects.js
+++ src/Projects.js
@@ -5,8 +5,12 @@ export default function Projects() {
   return (
     <div className="Projects">
       <h3>Projects</h3>
-      <p>Vanilla JS Weather App</p>
-      <p>React Weather App (include link to netlify and github)</p>
+      <div className="row">
+        <div className="col-sm-3"></div>
+        <div className="col-sm-4">Vanilla JS Weather App</div>
+        <div className="col-sm-4">React Weather App</div>
+        <div className="col-sm-1"></div>
+      </div>
     </div>
   );
 }
