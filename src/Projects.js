478fbe7e15e17132c2daf2e880df288a5a47ba7d
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-11-11 16:22:52 -0600

    Added styling

--- src/Projects.js
+++ src/Projects.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import "./Projects.css";
 
 export default function Projects() {
@@ -6,8 +7,11 @@ export default function Projects() {
     <div className="Projects">
       <h3>Projects</h3>
       <div className="row">
-        <div className="col-sm-3"></div>
-        <div className="col-sm-4">Vanilla JS Weather App</div>
+        <div className="col-sm-2"></div>
+        <div className="col-sm-4">
+          <div>Vanilla JS Weather App</div>
+          <div>Hello</div>
+        </div>
         <div className="col-sm-4">React Weather App</div>
         <div className="col-sm-1"></div>
       </div>
