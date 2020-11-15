291183a6c76b5c1bd0c0e69016337e5950ed8318
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-11-15 15:16:09 -0600

    Added columns

--- src/Technologies.js
+++ src/Technologies.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Editor from "./images/VS-Code.png";
-import API from "./images/API.png";
+import VS from "./images/VS-Code.png";
+import API from "./images/API-image.png";
 
 import Hosting from "./images/Hosting.png";
 
@@ -11,31 +11,53 @@ export default function Technologies() {
   return (
     <div className="Technologies">
       <h3>Technologies</h3>
-      <span className="html">
-        <i className="fab fa-html5 fa-5x "></i>
-      </span>
-      <span className="css">
-        <i className="fab fa-css3-alt fa-5x"></i>
-      </span>
-
-      <span className="javascript">
-        <i className="fab fa-js-square fa-5x"></i>
-      </span>
-      <img src={Editor} alt="" width={80}></img>
-      <img src={API} alt="" width={100}></img>
-
-      <span className="github">
-        <i class="fab fa-github fa-5x"></i>
-      </span>
-      <span className="bootstrap">
-        <i class="fab fa-bootstrap fa-5x"></i>
-      </span>
-
-      <img src={Hosting} alt="" width={100}></img>
-
-      <span className="react">
-        <i class="fab fa-react fa-5x"></i>
-      </span>
+      <div className="row">
+        <div className="col-sm-1">
+          <span className="html">
+            <i className="fab fa-html5 fa-5x "></i>
+            <p>HTML</p>
+          </span>
+        </div>
+        <div className="col-sm-1">
+          <span className="css">
+            <i className="fab fa-css3-alt fa-5x"></i>
+            <p>CSS</p>
+          </span>
+        </div>
+        <div className="col-sm-1">
+          <span className="javascript">
+            <i className="fab fa-js-square fa-5x"></i>
+            <p>JavaScript</p>
+          </span>
+        </div>
+        <div className="col-sm-1">
+          <img src={VS} alt="" width={80}></img>
+        </div>
+        <div className="col-sm-1">
+          <img src={API} alt="" width={80}></img>
+        </div>
+        <div className="col-sm-1">
+          <span className="github">
+            <i class="fab fa-github fa-5x"></i>
+          </span>
+        </div>
+        <div className="col-sm-1">
+          <span className="bootstrap">
+            <i class="fab fa-bootstrap fa-5x"></i>
+          </span>
+        </div>
+        <div className="col-sm-1">
+          <span className="hosting">
+            {" "}
+            <i class="fas fa-laptop-code fa-4x"></i>
+          </span>
+        </div>
+        <div className="col-sm-1">
+          <span className="react">
+            <i class="fab fa-react fa-5x"></i>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
