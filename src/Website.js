c0a68e63845ecafc6e6ef5105cd0de2339d00658
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-11-17 12:47:19 -0600

    Added new component

---
+++ src/Website.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Body from "./Body";
+import Footer from "./Footer";
+
+export default function Website() {
+  return (
+    <div className="Website">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
