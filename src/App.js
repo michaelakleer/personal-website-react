6f502921b7b73ed8c6ccde8b8c6f4a77c0b87714
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-11-22 15:56:07 -0600

    Changed or removed unnecessary files/images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
-import "react-bulma-components/dist/react-bulma-components.min.css";
-
-import Navbar from "./Navbar";
-import Body from "./Body";
-import Footer from "./Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 import "./App.css";
 
@@ -11,8 +7,6 @@ export default function App() {
   return (
     <div className="App">
       <Navbar />
-      <Body />
-      <Footer />
     </div>
   );
 }
