7227eec478f6dce66f98c6b7ea82272bb980e200
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-11-27 16:40:48 -0600

    Created new components

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <section className="Home">
+      <div className="intro">
+        <h1>Michaela Kleer</h1>
+        <h2>Front-end Web Developer</h2>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Projects.css";
+
+export default function Projects() {
+  return (
+    <section className="Projects">
+      <h1>Projects</h1>
+      <p>Vanilla Javascript Weather App</p>
+      <p>React Weather App</p>
+    </section>
+  );
+}
