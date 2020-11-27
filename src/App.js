e2d2b698d1e7199ac8467476a1285497dbf3928d
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-11-27 16:40:57 -0600

    Imported new components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import Body from "./Body";
+import Home from "./components/Home/Home";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Body />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Home />
+        <AboutMe />
+        <Projects />
+        <Footer />
+      </div>
+    </Router>
   );
 }
