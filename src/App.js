ca9289aaa47a3a71fa046be0757f59282d96c8c2
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-11-27 20:25:48 -0600

    Added scroll from react-router-dom

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home/Home";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
@@ -12,10 +13,13 @@ export default function App() {
   return (
     <Router>
       <div className="App">
+        <ScrollToTop />
         <Navbar />
-        <Home />
-        <AboutMe />
-        <Projects />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about-me" exact component={AboutMe} />
+          <Route path="/projects" exact component={Projects} />
+        </Switch>
         <Footer />
       </div>
     </Router>
