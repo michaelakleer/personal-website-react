55075ebc2c36f4aa4ba665f71d0b0b5637fec107
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2020-12-02 17:28:03 -0600

    Added new information

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,6 +7,8 @@ export default function Projects() {
       <h1>Projects</h1>
       <p>Vanilla Javascript Weather App</p>
       <p>React Weather App</p>
+      <br />
+      <h2>In progress</h2>
     </section>
   );
 }
