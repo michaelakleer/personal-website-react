fcfc8f5e2b16f991375b26963904a3976f05bbcb
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-01-11 15:45:28 -0600

    Updated info

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,13 @@ import "./Projects.css";
 export default function Projects() {
   return (
     <section className="Projects">
-      <h1>Projects</h1>
+      <h1>Portfolio</h1>
+      <br />
+      <h2> Commissioned Work</h2>
+      <p>Keeping Rural Heathcare Close to Home</p>
+      <p>Portfolio</p>
+      <br />
+      <h2>Projects</h2>
       <p>Vanilla Javascript Weather App</p>
       <p>React Weather App</p>
       <br />
