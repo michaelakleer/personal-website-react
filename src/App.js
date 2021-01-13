251d76f8852a37f619fe1ab514d847525d545e3b
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-01-12 21:08:28 -0600

    Added route to contact page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -19,6 +20,7 @@ export default function App() {
           <Route path="/" exact component={Home} />
           <Route path="/about-me" exact component={AboutMe} />
           <Route path="/projects" exact component={Projects} />
+          <Route path="/contact" exact component={Contact} />
         </Switch>
         <Footer />
       </div>
